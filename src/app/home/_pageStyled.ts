5b184056295aa01edc2733b43e0fe56bb693e602
Jacob Andes <jacob.andes@gmail.com>
2023-10-31 20:01:52 -0700

    style: Home page minimal styling

---
+++ src/app/home/_pageStyled.ts
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+const HomePageStyled = styled.main`
+    display: flex;
+    flex-direction: column;
+    padding: 24px;
+`;
+
+const TitleStyled = styled.h1``;
+
+const GreetingStyled = styled.p``;
+
+const HomeGridStyled = styled.section`
+    display: flex;
+    gap: 32px;
+    padding-top: 32px;
+    padding-left: 32px;
+`;
+
+const HomeLinkGroupStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    border: 2px solid #000000;
+    border-radius: 8px;
+    padding: 16px;
+`;
+
+const GroupTitleStyled = styled.h2``;
+
+const GroupListStyled = styled.ul`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    list-style: none;
+    padding: 0 8px 0 16px;
+    min-width: 180px;
+`;
+
+const GroupListItemStyled = styled.li``;
+
+const GroupListLinkStyled = styled(Link)`
+    text-decoration: none;
+    color: #000000;
+
+    &:hover {
+        text-decoration: underline;
+        font-weight: 600;
+    }
+`;
+
+export {
+    TitleStyled,
+    GreetingStyled,
+    HomePageStyled,
+    HomeGridStyled,
+    HomeLinkGroupStyled,
+    GroupTitleStyled,
+    GroupListStyled,
+    GroupListItemStyled,
+    GroupListLinkStyled,
+};
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,16 @@
-import Link from "next/link";
+"use client";
+
+import {
+    TitleStyled,
+    GreetingStyled,
+    HomeGridStyled,
+    HomePageStyled,
+    HomeLinkGroupStyled,
+    GroupTitleStyled,
+    GroupListStyled,
+    GroupListItemStyled,
+    GroupListLinkStyled,
+} from "./_pageStyled";
 import { testingIds } from "../../helpers";
 
 const userFirstName = "Jacob";
@@ -7,67 +19,79 @@ const homeIds = testingIds.pages.home;
 
 export default function HomePage() {
     return (
-        <main>
-            <h1 data-client-id={homeIds.title}>Home</h1>
-            <p data-client-id={homeIds.greeting}>Hey there, {userFirstName}</p>
-            <section>
-                <div data-client-id={homeIds.chooseActivitySection}>
-                    <h2 data-client-id={homeIds.chooseActivitySectionTitle}>
+        <HomePageStyled>
+            <TitleStyled data-client-id={homeIds.title}>Home</TitleStyled>
+            <GreetingStyled data-client-id={homeIds.greeting}>
+                Hey there, {userFirstName}!
+            </GreetingStyled>
+            <HomeGridStyled>
+                <HomeLinkGroupStyled data-client-id={homeIds.chooseActivitySection}>
+                    <GroupTitleStyled
+                        data-client-id={homeIds.chooseActivitySectionTitle}
+                    >
                         Activities
-                    </h2>
-                    <ul>
-                        <li>
-                            <Link
+                    </GroupTitleStyled>
+                    <GroupListStyled>
+                        <GroupListItemStyled>
+                            <GroupListLinkStyled
                                 href="/selectors"
                                 data-client-id={homeIds.chooseSelectors}
                             >
                                 Use the selectors
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/lists" data-client-id={homeIds.chooseLists}>
+                            </GroupListLinkStyled>
+                        </GroupListItemStyled>
+                        <GroupListItemStyled>
+                            <GroupListLinkStyled
+                                href="/lists"
+                                data-client-id={homeIds.chooseLists}
+                            >
                                 See your lists
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-                <div data-client-id={homeIds.manageActivitiesSection}>
-                    <h2 data-client-id={homeIds.manageActivitiesSectionTitle}>
+                            </GroupListLinkStyled>
+                        </GroupListItemStyled>
+                    </GroupListStyled>
+                </HomeLinkGroupStyled>
+                <HomeLinkGroupStyled data-client-id={homeIds.manageActivitiesSection}>
+                    <GroupTitleStyled
+                        data-client-id={homeIds.manageActivitiesSectionTitle}
+                    >
                         Manage
-                    </h2>
-                    <ul>
-                        <li>
-                            <Link
+                    </GroupTitleStyled>
+                    <GroupListStyled>
+                        <GroupListItemStyled>
+                            <GroupListLinkStyled
                                 href="/activities/new"
                                 data-client-id={homeIds.addActivity}
                             >
                                 Add an activity
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
+                            </GroupListLinkStyled>
+                        </GroupListItemStyled>
+                        <GroupListItemStyled>
+                            <GroupListLinkStyled
                                 href="/activities/edit"
                                 data-client-id={homeIds.editActivities}
                             >
                                 Edit your activities
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
+                            </GroupListLinkStyled>
+                        </GroupListItemStyled>
+                        <GroupListItemStyled>
+                            <GroupListLinkStyled
                                 href="/categories/edit"
                                 data-client-id={homeIds.editCategories}
                             >
                                 Edit your categories
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/account" data-client-id={homeIds.account}>
+                            </GroupListLinkStyled>
+                        </GroupListItemStyled>
+                        <GroupListItemStyled>
+                            <GroupListLinkStyled
+                                href="/account"
+                                data-client-id={homeIds.account}
+                            >
                                 Your account
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </section>
-        </main>
+                            </GroupListLinkStyled>
+                        </GroupListItemStyled>
+                    </GroupListStyled>
+                </HomeLinkGroupStyled>
+            </HomeGridStyled>
+        </HomePageStyled>
     );
 }
