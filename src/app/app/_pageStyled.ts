569d9f27f9f2d072a866d6191f0dd2d11acf000f
Jacob Andes <jacob.andes@gmail.com>
2025-07-31 19:58:55 -0700

    jandes-8: Logged in landing page

--- src/app/app/_pageStyled.ts
+++ src/app/app/_pageStyled.ts
@@ -1,4 +1,30 @@
 "use client";
-// import styled from "@emotion/styled";
+import styled from "@emotion/styled";
 
-export {};
+export const Page = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-inline: 32px;
+`;
+
+export const Greeting = styled.h1`
+    align-self: flex-start;
+`;
+
+export const MenuBoxes = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 60%;
+`;
+
+export const HomeBox = styled.section`
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    padding: 16px;
+`;
+
+export const BoxHeader = styled.h2``;
