7853d8ea255c73b1000fb82c591a25602d7db6fa
Jacob Andes <jacob.andes@gmail.com>
2026-01-10 00:05:29 -0800

    Add metadata from old project

---
+++ src/utils/constants/metadata.ts
@@ -0,0 +1,8 @@
+const WEBSITE_DESCRIPTION =
+    "Keep track of and choose which pleasurable activities to do!";
+const WEBSITE_NAME = "Happy Harmony";
+const WEBSITE_AUTHOR_NAME = "Jacob Andes";
+const WEBSITE_AUTHOR_EMAIL = "jacobandesdev@gmail.com";
+const WEBSITE_AUTHOR_WEBSITE = "jacobandes.dev";
+const WEBSITE_COMPOSITE_AUTHOR = `${WEBSITE_AUTHOR_NAME} ${WEBSITE_AUTHOR_EMAIL} ${WEBSITE_AUTHOR_WEBSITE}`;
+const WEBSITE_URL = "https://happyharmony.dev";
